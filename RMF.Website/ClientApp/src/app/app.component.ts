63ecc61e45e927bd4644a1a5ea9c014dcdda9385
Lawrence Schmid <lawrence.schmid@gmail.com>
2024-05-01 14:05:29 +0100

    Update app.component.ts

--- RMF.Website/ClientApp/src/app/app.component.ts
+++ RMF.Website/ClientApp/src/app/app.component.ts
@@ -17,7 +17,7 @@ export class AppComponent {
   private async getIpCountry() {
     // Check if visitor IP is from Ireland.
     var ipInfo = await this.httpClient.get<any>('https://jsonip.com').toPromise();
-    var countryInfo = await this.httpClient.get<any>('https://api.ipgeolocation.io/ipgeo?apiKey=fc5326eedc0b466caca7602fe9dbd12f&ip=' + (ipInfo !== null ? ipInfo.ip : '')).toPromise();
+    var countryInfo = await this.httpClient.get<any>('https://api.ipgeolocation.io/ipgeo?apiKey=<Your API Key>&ip=' + (ipInfo !== null ? ipInfo.ip : '')).toPromise();
     if (countryInfo !== null && countryInfo.country_name === 'Ireland')
       localStorage.setItem('isIreland', 'true');
     else
