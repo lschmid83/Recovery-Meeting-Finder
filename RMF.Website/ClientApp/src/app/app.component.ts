1c25ab9661d16508cb37b9184838f46020fd2a3c
Lawrence Schmid <lawrence.schmid@gmail.com>
2026-08-12 19:05:14 +0100

    removed check country

--- RMF.Website/ClientApp/src/app/app.component.ts
+++ RMF.Website/ClientApp/src/app/app.component.ts
@@ -11,20 +11,5 @@ import { HttpClient } from '@angular/common/http';
 export class AppComponent {
 
   constructor(private router: Router, private httpClient: HttpClient) {
-    this.getIpCountry();
-  }
-
-  private async getIpCountry() {
-    // Check if visitor IP is from Ireland.
-    var ipInfo = await this.httpClient.get<any>('https://jsonip.com').toPromise();
-    var countryInfo = await this.httpClient.get<any>('https://api.ipgeolocation.io/ipgeo?apiKey=4281037685db42b79075eb8555e175c3&ip=' + (ipInfo !== null ? ipInfo.ip : '')).toPromise();
-    if (countryInfo !== null && countryInfo.country_name === 'Ireland')
-      localStorage.setItem('isIreland', 'true');
-    else
-      localStorage.setItem('isIreland', 'false');
-
-    var mainSidebarItem = localStorage.getItem('mainSidebarItem');
-    if (mainSidebarItem === undefined)
-      localStorage.setItem('mainSidebarItem', 'about');
   }
 }
